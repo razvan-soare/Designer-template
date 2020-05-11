0ed5084415986b784f921402dcfd452bf822b827
razvan-soare <39434481+razvan-soare@users.noreply.github.com>
2020-05-11 08:11:28 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+// Welcome to the TypeScript Playground, this is a website
+// which gives you a chance to write, share and learn TypeScript.
+
+// You could think of it in three ways:
+//
+//  - A place to learn TypeScript in a place where nothing can break
+//  - A place to experiment with TypeScript syntax, and share the URLs with others
+//  - A sandbox to experiment with different compiler features of TypeScript
+
+const anExampleVariable = "Hello World2";
+console.log(anExampleVariable);
+
+// To learn more about the language, click above in "Examples" or "What's New".
+// Otherwise, get started by removing these comments and the world is your playground.
